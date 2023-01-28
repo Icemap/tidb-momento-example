4987b9425a198d8d3cfe214da92f242e06b3575c
Icemap <qizhi.wang@pingcap.com>
2023-01-28 12:06:23 +0800

    [Delete] untracked files

--- 2-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-ARTIFACT_BUCKET=$(cat bucket-name.txt)
-TEMPLATE=template.yml
-
-mvn clean package
-aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name java-read-aside --capabilities CAPABILITY_NAMED_IAM
